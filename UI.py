c97116045885b4261a3fcbc59d7a7c9232a170f8
Jyotsna <jyotsnasehgal24@gmail.com>
2023-12-15 02:00:15 -0500

    changes

--- UI.py
+++ UI.py
@@ -11,10 +11,20 @@ import pandas as pd
 import pickle
 from sklearn.preprocessing import StandardScaler
 import xgboost as xgb
+import shutil
 
 
-pytesseract.pytesseract.tesseract_cmd = None  # Update with your actual path
+pytesseract.pytesseract.tesseract_cmd = None
 
+# search for tesseract binary in path
+@st.cache_resource
+def find_tesseract_binary() -> str:
+    return shutil.which("tesseract")
+
+# set tesseract binary path
+pytesseract.pytesseract.tesseract_cmd = find_tesseract_binary()
+if not pytesseract.pytesseract.tesseract_cmd:
+    st.error("Tesseract binary not found in PATH. Please install Tesseract.")
 df=pd.read_csv(r'df')
 # Handling Image and converting to text
 
