dc84566f9d311bf851e761e958ea10e58c139962
Jyotsna <jyotsnasehgal24@gmail.com>
2024-03-21 14:55:07 -0400

    major upadates to project

---
+++ deployment.py
@@ -0,0 +1,8 @@
+# from pyngrok import ngrok 
+# !ngrok authtoken [Enter your authtoken here]
+# !nohup streamlit run app.py & 
+
+# url = ngrok.connect(port = 8501)
+# url #generates our URL
+
+# !streamlit run --server.port 80 app.py >/dev/null #
